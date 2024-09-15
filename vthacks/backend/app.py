82f6d85149d02462a8811e188d53061eb53788f9
Farhan Uddin <farhanuddin2006@gmail.com>
2024-09-15 03:14:00 -0400

    we are receiving values from the model now

--- vthacks/backend/app.py
+++ vthacks/backend/app.py
@@ -1,33 +1,77 @@
 import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 import random
 
+#from .backend import model
+
 app = Flask(__name__)
+
+# from ....VTHacks.backend import model
+
 cors = CORS(app, origins='*')
 
 
+def model(year, city):
+    data_path = "./all_cities_data_count_price.csv"  # Name of CSV file
+    energy_data = pd.read_csv(data_path)
+    currYear = 2024
+    dataMonths = (2024 - 2018) * 12
+
+    # Initializing X and Y Variables 
+    X = energy_data[['count']]
+    Y = energy_data[[city]]
+
+    # Declaring and Training the Model
+    linear = LinearRegression()
+    linear.fit(X, Y)
+
+    # Prepare input for prediction as a 2D array
+    input_data = np.array([[(year - currYear) * 12 + dataMonths]])
+
+    # Make prediction for the single input value
+    prediction = linear.predict(input_data)
+
+    # If you want to see the prediction for the entire DataFrame
+    # Calculate the prediction for each row based on the number of months
+    energy_data['Prediction'] = linear.predict(X)
+
+    # Print predictions for the whole DataFrame
+    #print(energy_data[['count', city, 'Prediction']])
+
+    # Print the prediction for the specific year
+    return prediction[0][0]
+
+#print(model(2030, "Boston"))
+
+city = "Boston"
+year = 2028
+
 @app.route('/search', methods=['GET'])
 def return_prediction():
+    
     return jsonify(
         {
             "year": [
                 2028
             ],
             "prediction": [
-                random.randint(3, 9),
-                random.randint(3, 9),
-                random.randint(3, 9),
-                random.randint(3, 9)
-               
+                model(year, city)
             ]
         }
     )
-
+    
 @app.route('/submit', methods=['POST'])
 def get_data():
     data = request.get_json()
-    print(data["city"])
+    city = data["city"]
+    year = data["year"]
+    print(year + " " + city)
+
     return data
         
     
