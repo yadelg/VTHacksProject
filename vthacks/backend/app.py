d5a6823e64cfcc9e1a179a1af351a90df370c649
Farhan Uddin <farhanuddin2006@gmail.com>
2024-09-15 02:12:11 -0400

    front end and back end communicating

--- vthacks/backend/app.py
+++ vthacks/backend/app.py
@@ -1,8 +1,36 @@
 import time
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import random
 
 app = Flask(__name__)
+cors = CORS(app, origins='*')
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+
+@app.route('/search', methods=['GET'])
+def return_prediction():
+    return jsonify(
+        {
+            "year": [
+                2028
+            ],
+            "prediction": [
+                random.randint(3, 9),
+                random.randint(3, 9),
+                random.randint(3, 9),
+                random.randint(3, 9)
+               
+            ]
+        }
+    )
+
+@app.route('/submit', methods=['POST'])
+def get_data():
+    data = request.get_json()
+    print(data["city"])
+    return data
+        
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+    
--- vthacks/src/App.js
+++ vthacks/src/App.js
@@ -1,32 +1,72 @@
-import logo from './logo.svg';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(3);
+  const [searchedYear, setSearchedYear] = useState(2028);
+  const [searchedCity, setSearchedCity] = useState("");
+  const [priceArray, setPriceArray] = useState([0, 0, 0, 0]);
+  const [submitted, setSubmitted] = useState(false);
+
+  const fetchAPI = async() => {
+    const response = await axios.get("http://localhost:5000/search");
+    setPriceArray(response.data.prediction);
+  }
+
+  const submit = async(e) => {
+    e.preventDefault();
+    let answers = {
+      "city": searchedCity,
+      "year": searchedYear
+    }
+    const response = await axios.post("http://localhost:5000/submit", answers, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    setSubmitted(true);
+  }
+
+  const handleCityDropDown = event => {
+    setSearchedCity(event.target.value);
+  }
+
+  const handleYearDropDown = event => {
+    setSearchedYear(event.target.value);
+  }
 
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    })
-  }, []);
+    fetchAPI();
+    setSubmitted(false);
+  }, [submitted]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Current time is: {currentTime}</p>
-      </header>
+      {
+        priceArray.map((price, index) => (
+          <span key={index}>{price},</span>
+        ))
+      }
+      <div>
+        <select onChange={handleCityDropDown}>
+          <option value="Boston">Boston</option>
+          <option value="New York">New York</option>
+          <option value="Atlanta">Atlanta</option>
+          <option value="Washington D.C.">Washington D.C.</option>
+        </select>
+        <select onChange={handleYearDropDown}>
+          <option value="2024">2024</option>
+          <option value="2025">2025</option>
+          <option value="2026">2026</option>
+          <option value="2027">2027</option>
+          <option value="2028">2028</option>
+        </select>
+        <br>
+        </br>
+        <button onClick={submit}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
