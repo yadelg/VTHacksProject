edd5ebcb2fa5d5b82f5b52acb8258771866bc8a9
unknown <jadenmoun@gmail.com>
2024-09-15 01:47:22 -0400

    Reworking linear regression model

--- backend.py
+++ backend.py
@@ -12,31 +12,29 @@ import sklearn # linear regression tool
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
-from bs4 import BeautifulSoup
-
 # Data fetching code -- Use pandas to read CSV
 
 # TEMPORARY, replace temp with CSV file name
-def model(year, dataset):
-    data_path = dataset # <--- name of CSV ex. electricity_perKWH.csv
+def model(year, city):
+    data_path = "./all_cities_data_count_price.csv" # <--- name of CSV ex. electricity_perKWH.csv
     energy_data = pd.read_csv(data_path)
-    currYear = 2023
-    dataMonths = (2023-2018) * 12
+    currYear = 2024
+    dataMonths = (2024-2018) * 12
 
     energy_data.head()
-    # plot comparing price over years to 
-    sns.scatterplot(x = 'Month', y = 'Price_Per_KWH', data = energy_data)
 
     # Initializing X and Y Variables 
-    X = energy_data[['Month']]
-    Y = energy_data[['Price_Per_KWH']]
+    X = energy_data[['count']]
+    Y = energy_data[[city]]
 
     # Declaring and Training the Model
     linear = LinearRegression()
     linear.fit(X, Y)
 
-    energy_data['Prediction'] = linear.predict((year - currYear) * 12 + dataMonths )
+    energy_data['Prediction'] = linear.predict(((year - currYear) * 12) + dataMonths)
+    print(energy_data['Prediction'])
 
+model(2030, "Boston")
 
 
 
