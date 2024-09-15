b644d1ff1d4e1f7c35028d00de8d139aca4b5561
Farhan Uddin <farhanuddin2006@gmail.com>
2024-09-15 05:28:42 -0400

    final edits

--- vthacks/src/App.js
+++ vthacks/src/App.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [searchedYear, setSearchedYear] = useState(2028);
-  const [searchedCity, setSearchedCity] = useState("");
+  const [searchedYear, setSearchedYear] = useState(2024);
+  const [searchedCity, setSearchedCity] = useState("Boston");
   const [price, setPrice] = useState(0);
   const [submitted, setSubmitted] = useState(false);
 
@@ -42,14 +42,34 @@ function App() {
 
   return (
     <div class="container">
-        <h1>Energy Price Prediction</h1>
+        <h1>⚡Amplytics⚡</h1>
+        <h3>Predicting Electricity Cost per kWh</h3>
         <div class="form-group">
             <label for="city-select">Select City:</label>
             <select id="city-select" onChange={handleCityDropDown}>
                 <option value="Boston">Boston</option>
                 <option value="New York">New York</option>
+                <option value="Philadelphia">Philadelphia</option>
+                <option value="Chicago">Chicago</option>
+                <option value="Detroit">Detroit</option>
+                <option value="Minneapolis">Minneapolis</option>
+                <option value="St. Louis">St. Louis</option>
                 <option value="Atlanta">Atlanta</option>
+                <option value="Baltimore">Baltimore</option>
+                <option value="Miami">Miami</option>
+                <option value="Tampa">Tampa</option>
                 <option value="Washington D.C.">Washington D.C.</option>
+                <option value="Dallas">Dallas</option>
+                <option value="Houston">Houston</option>
+                <option value="Denver">Denver</option>
+                <option value="Phoenix">Phoenix</option>
+                <option value="Los Angeles">Los Angeles	</option>
+                <option value="Riverside">Riverside</option>
+                <option value="San Diego">San Diego</option>
+                <option value="San Francisco">San Francisco</option>
+                <option value="Seattle">Seattle</option>
+                <option value="Urban Alaska">Urban Alaska</option>
+                <option value="Urban Hawaii">Urban Hawaii</option>
             </select>
         </div>
         <div class="form-group">
@@ -60,12 +80,24 @@ function App() {
                 <option value="2026">2026</option>
                 <option value="2027">2027</option>
                 <option value="2028">2028</option>
+                <option value="2029">2029</option>
+                <option value="2030">2030</option>
+                
             </select>
         </div>
         <button id="submit-button" onClick={submit}>Submit</button>
         <div id="result">
             <h2>Prediction:</h2>
-            <span id="price">{price}</span>
+            <span id="price">${price} per kWh</span>
+        </div>
+        <br></br>
+        <br></br>
+        <div>
+          <h2>How it works:</h2>
+          <p>Uses Linear Regression and past data on electrical costs in metropolitan areas 
+            in order to predict future costs for electricity in those areas
+            per kilowatt hour.
+          </p>
         </div>
     </div>
   );
